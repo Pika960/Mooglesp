3e786d778f4f6136525c49631de8bb7b8d432306
Gabriel Daenner <gabrieldaenner@googlemail.com>
2021-09-06 19:08:25 +0200

    RGBLed: Reworked fancy example

--- examples/RGBLedFancy/RGBLedFancy.ino
+++ examples/RGBLedFancy/RGBLedFancy.ino
@@ -11,10 +11,8 @@ const uint8_t pin_led_blue  = 27;
 //global objects
 Mooglesp_RGBLed rgbled(pin_led_red, pin_led_green, pin_led_blue);
 
-//global variables
-uint8_t nextState;
-
 //prototypes
+void doTask(uint8_t color, uint8_t task);
 void turnOff(uint32_t delay);
 
 /**
@@ -34,61 +32,14 @@ void setup()
  */
 void loop()
 {
-    //generate a random state
-    nextState = random(0, 7);
+    //turn the led with a random color on
+    doTask(random(0, 7), 0);
 
-    //let the led blink according to nextState
-    switch (nextState)
-    {
-        case 0:
-        {
-            Serial.printf("RGBLed will blink %d times in red\n", blinks);
-            rgbled.blink(blinks, 500, MOOGLESP_RGBLED_COLOR_RED);
-            Serial.printf("RGBLed blinked %d times in red\n", blinks);
-        } break;
+    //fade to a random color
+    doTask(random(0, 7), 1);
 
-        case 1:
-        {
-            Serial.printf("RGBLed will blink %d times in green\n", blinks);
-            rgbled.blink(blinks, 500, MOOGLESP_RGBLED_COLOR_GREEN);
-            Serial.printf("RGBLed blinked %d times in green\n", blinks);
-        } break;
-
-        case 2:
-        {
-            Serial.printf("RGBLed will blink %d times in blue\n", blinks);
-            rgbled.blink(blinks, 500, MOOGLESP_RGBLED_COLOR_BLUE);
-            Serial.printf("RGBLed blinked %d times in blue\n", blinks);
-        } break;
-
-        case 3:
-        {
-            Serial.printf("RGBLed will blink %d times in cyan\n", blinks);
-            rgbled.blink(blinks, 500, MOOGLESP_RGBLED_COLOR_CYAN);
-            Serial.printf("RGBLed blinked %d times in cyan\n", blinks);
-        } break;
-
-        case 4:
-        {
-            Serial.printf("RGBLed will blink %d times in magenta\n", blinks);
-            rgbled.blink(blinks, 500, MOOGLESP_RGBLED_COLOR_MAGENTA);
-            Serial.printf("RGBLed blinked %d times in magenta\n", blinks);
-        } break;
-
-        case 5:
-        {
-            Serial.printf("RGBLed will blink %d times in yellow\n", blinks);
-            rgbled.blink(blinks, 500, MOOGLESP_RGBLED_COLOR_YELLOW);
-            Serial.printf("RGBLed blinked %d times in yellow\n", blinks);
-        } break;
-
-        default:
-        {
-            Serial.printf("RGBLed will blink %d times in white\n", blinks);
-            rgbled.blink(blinks, 500, MOOGLESP_RGBLED_COLOR_WHITE);
-            Serial.printf("RGBLed blinked %d times in white\n", blinks);
-        } break;
-    }
+    //blink in a random color
+    doTask(random(0, 7), 2);
 
     //turn rgbled off for a second
     turnOff(1000);
@@ -114,6 +65,90 @@ void loop()
     turnOff(1000);
 }
 
+/**
+ * performs an action
+ * @param color the color value of the led
+ * @param task the task to be done
+ */
+void doTask(uint8_t color, uint8_t task)
+{
+    char     colorName[8] = "";
+    uint32_t colorCode    = 0;
+
+    switch (color)
+    {
+        case 0:
+        {
+            colorCode = MOOGLESP_RGBLED_COLOR_RED;
+            strcpy(colorName, "red");
+        } break;
+
+        case 1:
+        {
+            colorCode = MOOGLESP_RGBLED_COLOR_GREEN;
+            strcpy(colorName, "green");
+        } break;
+
+        case 2:
+        {
+            colorCode = MOOGLESP_RGBLED_COLOR_BLUE;
+            strcpy(colorName, "blue");
+        } break;
+
+        case 3:
+        {
+            colorCode = MOOGLESP_RGBLED_COLOR_CYAN;
+            strcpy(colorName, "cyan");
+        } break;
+
+        case 4:
+        {
+            colorCode = MOOGLESP_RGBLED_COLOR_MAGENTA;
+            strcpy(colorName, "magenta");
+        } break;
+
+        case 5:
+        {
+            colorCode = MOOGLESP_RGBLED_COLOR_YELLOW;
+            strcpy(colorName, "yellow");
+        } break;
+
+        default:
+        {
+            colorCode = MOOGLESP_RGBLED_COLOR_WHITE;
+            strcpy(colorName, "white");
+        } break;
+    }
+
+    switch (task)
+    {
+        case 0:
+        {
+            Serial.printf("RGBLed turned on with color %s\n", colorName);
+            rgbled.setColor(colorCode);
+            delay(1000);
+        } break;
+
+        case 1:
+        {
+            Serial.printf("RGBLed will now fade to color %s\n", colorName);
+            rgbled.fade(20, colorCode);
+            delay(1000);
+        } break;
+
+        case 2:
+        {
+            Serial.printf("RGBLed will blink %d times in %s\n", blinks, colorName);
+            rgbled.blink(blinks, 500, colorCode);
+            Serial.printf("RGBLed blinked %d times in %s\n", blinks, colorName);
+        } break;
+    }
+}
+
+/**
+ * turns the led off
+ * @param delay the timeframe to wait after the operation
+ */
 void turnOff(uint32_t delay)
 {
     rgbled.clear();
